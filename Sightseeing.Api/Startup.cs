5bfa9a3138918ca6672866f93888732fb199ffb6
Agata Borninska <ag.born@hotmail.com>
2021-03-11 18:02:06 +0100

    Added security info in Swagger

--- Sightseeing.Api/Startup.cs
+++ Sightseeing.Api/Startup.cs
@@ -37,6 +37,34 @@ namespace Sightseeing.Api
 
             services.AddSwaggerGen(c =>
             {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space] and then your token in the text input below.",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                      }
+                    });
+
                 c.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Version = "v1",
